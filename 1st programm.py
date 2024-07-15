483caedb9e13325d1b851446f6afde9a6fb134ec
EgorBlv <egor.zuev1@yandex.ru>
2024-07-15 14:16:33 +0300

    Add files via upload

---
+++ 1st programm.py
@@ -0,0 +1,4 @@
+print((9 ** 0.5) * 5)     # 1st program
+print(9.99 >= 9.98 and 1000 != 1000.1)  # 2nd program
+print((1234 % 1000 // 10) + (5678 % 5000 // 10))   # 3rd program
+print((int(13.42) == int(42.13 % 42 * 100)) and round(13.42 % 13 * 100) == int(42.13))   # 4th program
